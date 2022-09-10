73267df36a67a31e4dcea6d70bb1bc192d84f61f
Achref <medachrefg@gmail.com>
2022-09-10 19:51:48 +0100

    styling the header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png';
+import HeaderSocial from './HeaderSocial';
 const Header = () => {
 	return (
 		<header>
@@ -10,7 +11,11 @@ const Header = () => {
 				<h1>Ghozlene Mohamed Achref</h1>
 				<h5 className='text-light'>Full Stack developer</h5>
 				<CTA />
-				<img src={ME} alt='me' />
+				<HeaderSocial />
+				<div className='me'>
+					<img src={ME} alt='me' />
+				</div>
+
 				<a href='#contact' className='scroll__down'>
 					scroll Down
 				</a>
