17cb463d597e478053a40526e8332e616e750bc7
Achref <medachrefg@gmail.com>
2022-09-12 18:08:57 +0100

    working on footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
 import './Footer.css';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
 const Footer = () => {
-	return <section>Footer</section>;
+	return (
+		<footer>
+			<a href='#home' className='footer__logo'>
+				GHOZLENE
+			</a>
+			<ul className='premalinks'>
+				<li>
+					<a href='#home'>Home</a>
+				</li>
+
+				<li>
+					<a href='#about'>About</a>
+				</li>
+
+				<li>
+					<a href='#experience'>Experience</a>
+				</li>
+
+				<li>
+					<a href='#services'>Services</a>
+				</li>
+
+				<li>
+					<a href='#protfolio'>Protfolio</a>
+				</li>
+
+				<li>
+					<a href='#testimonials'>Testimonials</a>
+				</li>
+
+				<li>
+					<a href='#contact'>Contact</a>
+				</li>
+			</ul>
+			<div className='footer__socials'>
+				<a href='https://linkedin.com' target='_blank' rel='noreferrer'>
+					<BsLinkedin />
+				</a>
+				<a href='https://github.com' target='_blank' rel='noreferrer'>
+					<BsGithub />
+				</a>
+				<a href='https://facebook.com' target='_blank' rel='noreferrer'>
+					<BsFacebook />
+				</a>
+			</div>
+			<div className='footer__copyRight'>
+				<small>&copy;GHOZLENE med Achref all rights Reserved</small>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
