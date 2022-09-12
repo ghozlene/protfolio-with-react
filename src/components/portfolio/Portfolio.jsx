9d8fe71b1c1f66ae9fe1f0a0fb3982827ba56549
Achref <medachrefg@gmail.com>
2022-09-12 14:02:18 +0100

    fixing some issues and working on the protfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import './Portfolio.css';
+import data from './data';
+
 const Portfolio = () => {
-	return <section>Portfolio</section>;
+	return (
+		<section>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+			<div className='container portfolio__container'>
+				{data.map(({ id, image, title, github, demo }) => {
+					return (
+						<article className='portfolio__item' key={id}>
+							<div className='portfolio__item-image'>
+								<img src={image} alt={title} />
+							</div>
+							<h3>{title}</h3>
+							<div className='portfolio__item-cta'>
+								<a
+									href={github}
+									className='btn'
+									target='_blank'
+									rel='noreferrer'>
+									Github
+								</a>
+								<a
+									href={demo}
+									className='btn btn-primary'
+									target='_blank'
+									rel='noreferrer'>
+									Live Demo
+								</a>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
