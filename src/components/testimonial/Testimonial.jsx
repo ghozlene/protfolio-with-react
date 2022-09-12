2f88c572e1597b530960435e41f15d737b9d8dc3
Achref <medachrefg@gmail.com>
2022-09-12 15:39:03 +0100

    working on testimonial section

---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Testimonial.css';
+import data from './testimonial-data';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper';
+
+const Testimonial = () => {
+	return (
+		<section id='testimonials'>
+			<h5>Reviews from clients</h5>
+			<h2>testimonials</h2>
+			<Swiper
+				className='container testimonials__container'
+				effect={'coverflow'}
+				grabCursor={true}
+				centeredSlides={true}
+				slidesPerView={'auto'}
+				autoplay={{
+					delay: 1500,
+					disableOnInteraction: false,
+				}}
+				coverflowEffect={{
+					rotate: 50,
+					stretch: 0,
+					depth: 100,
+					modifier: 1,
+					slideShadows: true,
+				}}
+				pagination={true}
+				modules={[EffectCoverflow, Pagination, Autoplay]}>
+				{data.map(({ id, name, review, image }) => {
+					return (
+						<SwiperSlide className='testimonial' key={id}>
+							<div className='client__avatar'>
+								<img src={image} alt={name} />
+							</div>
+							<h5 className='client__name'>{name}</h5>
+							<small className='client__review'>{review}</small>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</section>
+	);
+};
+
+export default Testimonial;
---
+++ src/components/testimonial/testimonial-data.js
@@ -0,0 +1,37 @@
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+const data = [
+	{
+		id: 1,
+		image: AVTR1,
+		name: 'x1',
+		review:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, quae.',
+	},
+	{
+		id: 2,
+		image: AVTR2,
+		name: 'x2',
+		review:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, quae.',
+	},
+
+	{
+		id: 3,
+		image: AVTR3,
+		name: 'x3',
+		review:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, quae.',
+	},
+	{
+		id: 4,
+		image: AVTR4,
+		name: 'x4',
+		review:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, quae.',
+	},
+];
+
+export default data;
