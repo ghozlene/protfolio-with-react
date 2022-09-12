f6fc289ca1cf99048cc561575c9ae3b17a98909d
Achref <medachrefg@gmail.com>
2022-09-12 13:13:39 +0100

    working on the services section&styles

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,163 @@
 import React from 'react';
 import './Services.css';
+import { BsCheck2Circle } from 'react-icons/bs';
 const Services = () => {
-	return <section id='service'>Services</section>;
+	return (
+		<section id='services'>
+			<h5>what I Offer </h5>
+			<h2>Services</h2>
+			<div className='services__container'>
+				<article className='service'>
+					<div className='service__head'>
+						<h3>UI/UX design</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service '>
+					<div className='service__head'>
+						<h3>Web Developement</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Content Creation</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+
+						<li>
+							<BsCheck2Circle className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+								Dolores reiciendis in doloremque.
+							</p>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
 };
 
 export default Services;
